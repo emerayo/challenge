0fdb5af772b7c5cdf41771b5f99feef8b04b7e57
Emanuel Goldenberg <emanuelmerayo@hotmail.com>
2019-04-25 23:28:08 -0300

    Add tests for Router

---
+++ test/challenge/router_test.exs
@@ -0,0 +1,30 @@
+defmodule Challenge.RouterTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  @opts Challenge.Router.init([])
+
+  test "it returns the welcome message" do
+    # Create a test connection
+    conn = conn(:get, "/")
+
+    # Invoke the plug
+    conn = Challenge.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == Poison.encode!(%{response: "Welcome to our Bank API"})
+  end
+
+  test "it returns 404 when no route matches" do
+    # Create a test connection
+    conn = conn(:get, "/fail")
+
+    # Invoke the plug
+    conn = Challenge.Router.call(conn, @opts)
+
+    # Assert the response
+    assert conn.status == 404
+  end
+end
